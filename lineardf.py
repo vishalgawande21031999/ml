0bfb9f1dedfd2d863ddfbf7a81e8769f0c67321e
vishalgawande21031999 <48825418+vishalgawande21031999@users.noreply.github.com>
2019-09-02 17:20:24 +0530

    Add files via upload

---
+++ lineardf.py
@@ -0,0 +1,25 @@
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+
+import pandas as pd
+from sklearn import linear_model
+
+linear={'x':[1,2,3,4,5,6],'y':[1,2,3,4,5,6]}
+
+df=DataFrame(linear,columns=['x','y'])
+print(df)
+plt.scatter(df['x'], df['y'], color='red')
+plt.xlabel('x', fontsize=14)
+
+plt.ylabel('y', fontsize=14)
+plt.show()
+x=df[['x']]
+y=df[['y']]
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
+
+print('Intercept: \n', regr.intercept_)
+print('Coefficients: \n', regr.coef_)
+
+New_x=8
+print("new value is:",regr.predict([[x]]))
